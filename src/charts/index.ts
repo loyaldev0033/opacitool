2b4998846dfd52c1597cf62ac8a87de3d5632691
loyaldev322 <alexli930220@gmail.com>
2025-01-17 04:59:35 -0500

    fifth

--- src/charts/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CustomLineChart } from './CustomLineChart';
-export { default as CustomPieChart } from './CustomPieChart';
-export { default as CustomBarChart } from './CustomBarChart';
-export { default as CustomStackedBarChart } from './CustomStackedBarChart';
-export { default as CustomAreaChart } from './CustomAreaChart';
-export { default as CustomRadarChart } from './CustomRadarChart';
--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { Layout } from '@layout';
-import { AnalyticsPage, OrdersPage, OverviewPage, ProductsPage, SalesPage, SettingsPage, UsersPage } from '@pages';
+import { OverviewPage} from '@pages';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<Layout />}>
             <Route index element={<OverviewPage />} />
-            <Route path='products' element={<ProductsPage />} />
+            {/* <Route path='products' element={<ProductsPage />} />
             <Route path='users' element={<UsersPage />} />
             <Route path='orders' element={<OrdersPage />} />
             <Route path='sales' element={<SalesPage />} />
             <Route path='analytics' element={<AnalyticsPage />} />
-            <Route path='settings' element={<SettingsPage />} />
+            <Route path='settings' element={<SettingsPage />} /> */}
         </Route>
     ),
     {
